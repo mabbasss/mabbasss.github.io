356086fad6a26671abd13d7f52103ebce46cfa95
mabbasss <mabbasss@users.noreply.github.com>
2021-06-11 21:43:46 +0200

    Tech House

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,112 +1,17 @@
 const productsApi ="https://afternoon-falls-30227.herokuapp.com/api/v1/products/";
 
-const xhr = new XMLHttpRequest();
+
+showHome();
+
+function showHome(){
+    const xhr = new XMLHttpRequest();
 
 xhr.open("GET",productsApi);
 
 xhr.send();
-
 xhr.onload = function(){
-    
-    if(xhr.status == 200){
-        const products = JSON.parse(xhr.response).data;
-        console.log(products);
-
-        const productsList = document.getElementById("productsList");
-
-        products.forEach(product => {
-
-            var productDiv = document.createElement('div');
-            var productNames = document.createElement('h1');
-            var productImg = document.createElement('img');
-            var productPrices = document.createElement('p');
-            var productAdd = document.createElement('p');
-            var addBtn = document.createElement('button');
-            var a = product.ProductId;
-
-            productDiv.classList.add("card");
-            productDiv.classList.add("card");
-            productDiv.classList.add("mt-3");
-            productPrices.classList.add("price");
-
-            productImg.src = product.ProductPicUrl;
-            productNames.innerText = product.Name;
-            productPrices.innerText = `$${product.Price}`;
-            addBtn.innerText = "Add to Cart"
 
 
-            productDiv.appendChild(productImg);
-            productDiv.appendChild(productNames);
-            productDiv.appendChild(productPrices);
-            productDiv.appendChild(productAdd);
-            productAdd.appendChild(addBtn);
-            productsList.appendChild(productDiv);
-
-            
-        productImg.addEventListener('click',function(){
-            
-            var xhr2=new XMLHttpRequest();
-            
-            xhr2.open('GET',"https://afternoon-falls-30227.herokuapp.com/api/v1/products/"+a+"")
-            xhr2.send();
-            
-            let singleProduct='';
-            xhr2.onload=function(){
-                var data2=xhr2.response
-                var id=JSON.parse(data2).data
-                console.log(id)
-
-                singleProduct+=`
-                <div class="container px-4 px-lg-5 my-5">
-                <div class="row gx-4 gx-lg-5 align-items-center shadow-lg p-3 mb-5 rounded" style="background: white;">
-                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="${id.ProductPicUrl}" alt="..." /></div>
-                    <div class="col-md-6">
-                        <div class="small mb-1">${id.ProductId}</div>
-                        <h1 class="display-5 fw-bolder">${id.Name}</h1>
-                        <div class="fs-5 mb-5">
-                            <span class="text-decoration-line-through">$ ${id.Price}</span>
-                            <span>$40.00</span>
-                        </div>
-                        <p class="lead">${id.Description}</p>
-                        <div class="d-flex">
-                            <input class="form-control text-center me-3" id="inputQuantity" type="number" value="1" min="1" max="${id.Quantity}" style="max-width: 5rem"/>
-                            <button class="btn btn-outline-dark flex-shrink-0" type="button">
-                                <i class="bi-cart-fill me-1"></i>
-                                Add to cart
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                `  
-                productsList.innerHTML=singleProduct
-                
-                
-
-
-
-
-            }
-
-            
-
-
-            
-        })
-        })
-        
-        cont2.style.display = "none"
-        cont3.style.display = "none"
-        cont4.style.display = "none"
-
-    } else {
-        console.log("Something went wrong");
-    }
-}
-
-
-
-function showHome(){
     const products = JSON.parse(xhr.response).data;
         console.log(products);
     const productsList = document.getElementById("productsList");
@@ -121,9 +26,11 @@ function showHome(){
             var addBtn = document.createElement('button');
             var a = product.ProductId;
 
+
             productDiv.classList.add("card");
             productDiv.classList.add("mt-3");
             productPrices.classList.add("price");
+            addBtn.classList.add('add-cart');
             productImg.src = product.ProductPicUrl;
             productNames.innerText = product.Name;
             productPrices.innerText = `$${product.Price}`;
@@ -137,6 +44,47 @@ function showHome(){
             productAdd.appendChild(addBtn);
             productsList.appendChild(productDiv);
 
+
+            
+        addBtn.addEventListener('click',function(){
+            cartNumbers();
+            addProduct();
+
+
+        })
+
+        function addProduct(){
+
+            let productId = localStorage.getItem(product.ProductId);
+            console.log(productId);
+
+            if(productId){
+                const productItem = JSON.parse(localStorage.getItem(product.ProductId));
+                productItem.inCart++;
+                localStorage.setItem(product.ProductId,JSON.stringify(productItem));
+            } else {
+                product.inCart=1;
+                localStorage.setItem(product.ProductId,JSON.stringify(product));
+                // document.getElementById().innerText = 1;
+            }
+
+        }
+
+        function cartNumbers(){
+            let productNumbers = localStorage.getItem('cartNumber');
+            productNumbers = parseInt(productNumbers);
+
+            if(productNumbers){
+                localStorage.setItem('cartNumber',productNumbers + 1);
+                document.getElementById('lblCartCount').innerText = productNumbers + 1;
+        
+            } else {
+                localStorage.setItem('cartNumber',1);
+                document.getElementById('lblCartCount').innerText = 1;
+            }
+        }
+
+        
             productImg.addEventListener('click',function(){
             
                 var xhr2=new XMLHttpRequest();
@@ -164,7 +112,7 @@ function showHome(){
                                     <p class="lead">${id.Description}</p>
                                     <div class="d-flex">
                                         <input class="form-control text-center me-3" id="inputQuantity" type="number" value="1" min="1" max="${id.Quantity}" style="max-width: 5rem" />
-                                        <button class="btn btn-outline-dark flex-shrink-0" type="button">
+                                        <button id="addcart" class="add-cart btn btn-outline-dark flex-shrink-0" type="button">
                                             <i class="bi-cart-fill me-1"></i>
                                             Add to cart
                                         </button>
@@ -175,7 +123,44 @@ function showHome(){
                     `  
                     productsList.innerHTML=singleProduct
                     
+                    const add = document.getElementById("addcart");
+                    add.addEventListener('click',function(){
+                        cartNumbers();
+                        addProduct();
+                    })
+
+                    function cartNumbers(){
+                        let productNumbers = localStorage.getItem('cartNumber');
+                        productNumbers = parseInt(productNumbers);
+                        if(productNumbers){
+                            localStorage.setItem('cartNumber',productNumbers + 1);
+                            document.getElementById('lblCartCount').innerText = productNumbers + 1;
                     
+                        } else {
+                            localStorage.setItem('cartNumber',1);
+                            document.getElementById('lblCartCount').innerText = 1;
+                        }
+                    }
+
+
+                    function addProduct(){
+
+                        let productId = localStorage.getItem(product.ProductId);
+                        console.log(productId);
+            
+                        if(productId){
+                            const productItem = JSON.parse(localStorage.getItem(product.ProductId));
+                            productItem.inCart++;
+                            localStorage.setItem(product.ProductId,JSON.stringify(productItem));
+                        } else {
+                            product.inCart=1;
+                            localStorage.setItem(product.ProductId,JSON.stringify(product));
+                            // document.getElementById().innerText = 1;
+                        }
+            
+                    }
+            
+
                 }
     
                 
@@ -184,6 +169,14 @@ function showHome(){
                 
             })
         })
+
+
+
+
+        cont2.style.display = "none"
+        cont3.style.display = "none"
+        cont4.style.display = "none"
+}
 }
 
 
@@ -192,7 +185,9 @@ function showHome(){
 
 
 
-
+// $(document).ready(function() {  
+//     $('#cartModal').modal('show');
+//   });
 
 
 
@@ -367,5 +362,4 @@ contactForm.addEventListener('submit',(e)=>{
 
 
 })
-
 
